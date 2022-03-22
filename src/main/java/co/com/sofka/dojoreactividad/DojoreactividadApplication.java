a202edece3889e4a89b529569dfa20cbf9d58349
CamiloMonje <lumonje12@gmail.com>
2022-03-22 11:18:08 -0500

    Se agregan las clases -Contarcorreos -ContarCorreosPorDominio -Filter

--- src/main/java/co/com/sofka/dojoreactividad/DojoreactividadApplication.java
+++ src/main/java/co/com/sofka/dojoreactividad/DojoreactividadApplication.java
@@ -1,15 +1,29 @@
 package co.com.sofka.dojoreactividad;
 
+import co.com.sofka.dojoreactividad.PuntoUno.operadores.model.ContarCorreos;
+import co.com.sofka.dojoreactividad.PuntoUno.operadores.model.ContarCorreosPorDominio;
+import co.com.sofka.dojoreactividad.PuntoUno.operadores.model.Filter;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DojoreactividadApplication {
+public class DojoreactividadApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DojoreactividadApplication.class, args);
 	}
 
-	
 
+	@Override
+	public void run(String... args) throws Exception {
+//		Filter app = new Filter();
+//		app.filter();
+
+//		ContarCorreos contar = new ContarCorreos();
+//		contar.contarCorreos();
+
+		ContarCorreosPorDominio contarCorreosPorDominio = new ContarCorreosPorDominio();
+		contarCorreosPorDominio.contarCorreosPorDominio();
+	}
 }
