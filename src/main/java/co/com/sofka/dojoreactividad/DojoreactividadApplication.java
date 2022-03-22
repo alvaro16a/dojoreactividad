f140105ec00a5e258328611632d431fe19e2930e
Alvaro Mena <alvaro16a@gmail.com>
2022-03-22 13:28:03 -0500

    se realizo el segundo punto

--- src/main/java/co/com/sofka/dojoreactividad/DojoreactividadApplication.java
+++ src/main/java/co/com/sofka/dojoreactividad/DojoreactividadApplication.java
@@ -1,10 +1,13 @@
 package co.com.sofka.dojoreactividad;
 
 
+import co.com.sofka.dojoreactividad.PuntoDos.Chat;
 import co.com.sofka.dojoreactividad.PuntoUno.operadores.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 
 @SpringBootApplication
@@ -17,11 +20,14 @@ public class DojoreactividadApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		Scanner sc = new Scanner(System.in);
+
     	//istinct distinto = new Distinct();
 		//distinto.distinct();
 
-		Map mapeo = new Map();
-		mapeo.map();
+		//Map mapeo = new Map();
+		//mapeo.map();
     
 		//Filter app = new Filter();
 		//app.filter();
@@ -31,5 +37,9 @@ public class DojoreactividadApplication implements CommandLineRunner {
 
 		//ContarCorreosPorDominio contarCorreosPorDominio = new ContarCorreosPorDominio();
 		//contarCorreosPorDominio.contarCorreosPorDominio();
+
+		Chat chat = new Chat();
+		System.out.println("Por favor ingrese el mensaja a enviar: ");
+		chat.chat(sc.nextLine());
 	}
 }
--- src/main/java/co/com/sofka/dojoreactividad/PuntoDos/Chat.java
+++ src/main/java/co/com/sofka/dojoreactividad/PuntoDos/Chat.java
@@ -1,14 +1,22 @@
 package co.com.sofka.dojoreactividad.PuntoDos;
 
+import co.com.sofka.dojoreactividad.PuntoUno.operadores.Map;
 import co.com.sofka.dojoreactividad.PuntoUno.operadores.model.Correo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Chat {
 
-    public void chat() {
+    private static final Logger log = LoggerFactory.getLogger(Chat.class);
+
+    public void chat(String mensaje) {
+
 
         List<String> malasPalabras= new ArrayList<>();
             malasPalabras.add("cafre");
@@ -16,23 +24,26 @@ public class Chat {
             malasPalabras.add("baboso");
             malasPalabras.add("mojon");
             malasPalabras.add("ramera");
-            malasPalabras.add("Gusano");
+            malasPalabras.add("gusano");
             malasPalabras.add("cabestro");
-            malasPalabras.add("Ladilla");
+            malasPalabras.add("ladilla");
             malasPalabras.add("capullo");
 
-        List<String> mensajes= new ArrayList<>();
-            mensajes.add("hola como estas");
-            mensajes.add("sos un mojon pedazo de capullo");
+        Mono<String> monoMensaje= Mono.just(mensaje);
+          monoMensaje.subscribe(txt -> log.info(txt));
 
-            Flux<String> mensajesCorregidos=Flux.fromIterable(mensajes)
-                    .map(mensaje -> {
-                        malasPalabras.stream().map(palabra -> {
-                            mensaje.replaceAll(palabra,"xxxx");
-                            return mensaje;
-                        });
-                       return mensaje;
-                    });
-        mensajesCorregidos.subscribe(correo-> log.info(correo.toString()));
+
+        List<String> palabras =  Arrays.asList(mensaje.split(" "));
+        Flux<String> mensajeFlujo = Flux.fromIterable(palabras);
+        Flux<String> mensajeCorregido = mensajeFlujo.map(palabra ->{
+            if(malasPalabras .contains(palabra)){
+                return palabra.replace(palabra, "xxxxxx");
+            }
+            return palabra;
+        });
+        
+        mensajeCorregido.collectList().subscribe(x-> log.info(x+" "));
+
     }
 }
+
